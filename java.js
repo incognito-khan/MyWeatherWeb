cc9bf9f148788913dd494e2c4f27027a6901db71
Abdullah Nadeem <104924536+myweatherweb@users.noreply.github.com>
2022-12-08 07:18:02 -0800

    Update java.js

--- java.js
+++ java.js
@@ -1,3 +1,12 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-FMNFKKRTN0"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-FMNFKKRTN0');
+</script>
 const options = {
     method: 'GET',
     headers: {
